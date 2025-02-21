bb41b6eacfa6a69fc586b0f46732f5abd6c3a480
Clément <clementfiesta@gmail.com>
2025-02-21 09:41:12 +0100

    Add COMMAND_PREFIX to .env

--- src/modules/discord/events/guild/messageCreate.ts
+++ src/modules/discord/events/guild/messageCreate.ts
@@ -1,21 +1,30 @@
 import AbstractEvent from '@discord/misc/AbstractEvent';
 import { DiscordClientService } from '@discord/services/discord-client.service';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Message } from 'discord.js';
 
-const PREFIX = '!';
+const DEFAULT_PREFIX = '!';
 
 @Injectable()
 export class MessageCreateEvent extends AbstractEvent {
-  constructor(private readonly discordClientService: DiscordClientService) {
+  constructor(
+    private readonly discordClientService: DiscordClientService,
+    private readonly configService: ConfigService,
+  ) {
     super('messageCreate', false);
   }
 
   public execute(message: Message) {
     if (message.author.bot) return;
 
-    if (message.content.startsWith(PREFIX)) {
-      const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+    const prefix = this.configService.get<string>(
+      'COMMAND_PREFIX',
+      DEFAULT_PREFIX,
+    );
+
+    if (message.content.startsWith(prefix)) {
+      const args = message.content.slice(prefix.length).trim().split(/ +/g);
       const commandName = args.shift()?.toLowerCase();
       if (!commandName) return;
 
